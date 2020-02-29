20d67f1b968c0d4339664389543c3d72c1a3e33f
zibrahim2046 <zibrahim2046@gmail.com>
2020-02-29 16:11:33 -0600

    test

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -98,16 +98,72 @@ var trivia = {
     $("#game").show();
 
     //empty last results
-    $('results').html('');
+    $("results").html("");
 
     //show timer
-  $("#timer").text(trivia.timer);
+    $("#timer").text(trivia.timer);
 
-  //remove start button
-  $("#start").hide();
-  $("remaining-time").show();
+    //remove start button
+    $("#start").hide();
+    $("remaining-time").show();
 
+    // ask first question
+    trivia.nextQuestion();
+  },
+  // method to loop through and display questions and options
+  nextQuestion: function() {
+    // set timer to 20 seconds each question
+    trivia.timer = 10;
+    $("#timer").removeClass("last-seconds");
+    $("#timer").text(trivia.timer);
+
+    // to prevent timer speed up
+    if (!trivia.timerOn) {
+      trivia.timerId = setInterval(trivia.timerRunning, 1000);
+    }
+
+    // gets all the questions then indexes the current questions
+    var questionContent = Object.values(trivia.questions)[trivia.currentSet];
+    $("#question").text(questionContent);
+
+    // an array of all the user options for the current question
+    var questionOptions = Object.values(trivia.options)[trivia.currentSet];
+
+    // creates all the trivia guess options in the html
+    $.each(questionOptions, function(index, key) {
+      $("#options").append(
+        $('<button class="option btn btn-info btn-lg">' + key + "</button>")
+      );
+    });
+  },
+  // method to decrement counter and count unanswered if timer runs out
+  timerRunning: function() {
+    // if timer still has time left and there are still questions left to ask
+    if (
+      trivia.timer > -1 &&
+      trivia.currentSet < Object.keys(trivia.questions).length
+    ) {
+      $("#timer").text(trivia.timer);
+      trivia.timer--;
+      if (trivia.timer === 4) {
+        $("#timer").addClass("last-seconds");
+      }
+    }
+    // the time has run out and increment unanswered, run result
+    else if (trivia.timer === -1) {
+      trivia.unanswered++;
+      trivia.result = false;
+      clearInterval(trivia.timerId);
+      resultId = setTimeout(trivia.guessResult, 1000);
+      $("#results").html(
+        "<h3>Out of time! The answer was " +
+          Object.values(trivia.answers)[trivia.currentSet] +
+          "</h3>"
+      )}
+      // if all the questions have been shown, end the game and show reults
+      else if(trivia.currentSet === Object.keys(trivia.questions).length) {
+
+      
+    }
   }
-
-  
 };
